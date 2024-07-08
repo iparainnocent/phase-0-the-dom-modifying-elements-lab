ebd8210461d23ee64503f0591219f41caccf8560
iparainnocent <innocent.ipara@student.moringaschool.com>
2024-07-08 11:40:58 +0300

    add index.html, index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+const main = document.getElementById('main').remove()
+
+
+const newHeader = document.createElement('h1')
+
+newHeader.id = 'victory';
+
+newHeader.innerHTML = 'YOUR NAME is the champion';
+
+document.body.appendChild(newHeader);
+
+
+
